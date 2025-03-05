1283d87e0d26df7fd4abb4ae898f0f0b406adbe2
Saurabh Deulkar <saurabhdeulkar11@gmail.com>
2025-03-05 12:52:35 +0000

    Code Update in Lab4

--- Lab4/priorityqueue.c
+++ Lab4/priorityqueue.c
@@ -1,7 +1,3 @@
-// Implement the hospital emergency queueu to cate service to patients based on 
-// Serious (top priority), Non serious (Medium priority and General (Low priority)
-// General checkup (least priority)
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -23,14 +19,14 @@ struct patient* newPatient(int id, int priority) {
 
 // Function to insert a patient into the queue
 void insertPatient(struct patient** head, int id, int priority) {
-    struct patient* start = (*head);
     struct patient* temp = newPatient(id, priority);
 
-    if ((*head)->priority > priority) {
+    if (*head == NULL || (*head)->priority > priority) {
         temp->next = *head;
-        (*head) = temp;
+        *head = temp;
     } else {
-        while (start->next != NULL && start->next->priority < priority) {
+        struct patient* start = *head;
+        while (start->next != NULL && start->next->priority <= priority) {
             start = start->next;
         }
         temp->next = start->next;
@@ -40,34 +36,64 @@ void insertPatient(struct patient** head, int id, int priority) {
 
 // Function to remove a patient from the queue
 void removePatient(struct patient** head) {
+    if (*head == NULL) {
+        printf("No patients in the queue.\n");
+        return;
+    }
     struct patient* temp = *head;
-    (*head) = (*head)->next;
+    printf("Patient ID %d with priority %d has been attended.\n", temp->id, temp->priority);
+    *head = (*head)->next;
     free(temp);
 }
 
 // Function to display the queue
 void displayQueue(struct patient* head) {
+    if (!head) {
+        printf("No patients in the queue.\n");
+        return;
+    }
+    printf("Current patient queue:\n");
     while (head) {
         printf("Patient ID: %d, Priority: %d\n", head->id, head->priority);
         head = head->next;
     }
 }
 
+// Main function to drive the user interaction
 int main() {
-    struct patient* head = newPatient(1, 2);
-    insertPatient(&head, 2, 1);
-    insertPatient(&head, 3, 3);
-    insertPatient(&head, 4, 2);
-    insertPatient(&head, 5, 1);
-    insertPatient(&head, 6, 3);
+    struct patient* head = NULL;
+    int choice, id, priority;
 
-    printf("Queue before removing a patient:\n");
-    displayQueue(head);
+    while (1) {
+        printf("\nHospital Emergency Queue System\n");
+        printf("1. Add patient\n");
+        printf("2. Attend a patient (Remove highest priority)\n");
+        printf("3. Display queue\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-    removePatient(&head);
-
-    printf("\nQueue after removing a patient:\n");
-    displayQueue(head);
+        switch (choice) {
+            case 1:
+                printf("Enter Patient ID: ");
+                scanf("%d", &id);
+                printf("Enter Priority (1-Serious, 2-Non serious, 3-General, 4-General Checkup): ");
+                scanf("%d", &priority);
+                insertPatient(&head, id, priority);
+                break;
+            case 2:
+                removePatient(&head);
+                break;
+            case 3:
+                displayQueue(head);
+                break;
+            case 4:
+                printf("Exiting system.\n");
+                return 0;
+            default:
+                printf("Invalid choice! Try again.\n");
+        }
+    }
 
     return 0;
 }
